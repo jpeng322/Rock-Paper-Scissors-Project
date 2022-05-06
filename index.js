4f8615696297a9b41559da2daac7c84ee76c641f
jacky <jackypengsmail@gmail.com>
2022-05-05 20:35:35 -0400

    implemented playRound function for result of a round of gameplay

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-let options = ["Rock", "Paper", "Scissor"]
+// let options = ["Rock", "Paper", "Scissor"]
 
-function computerPlay(a) {
-    console.log(a[(Math.floor(Math.random() * 3))])
+// function computerPlay(a) {
+//     console.log(a[(Math.floor(Math.random() * 3))])
+// }
+
+// computerPlay(options)
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if (playerSelection === "rock" && computerSelection === "scissor") {
+        return "Congratulations! You Won"
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        return "You tied! Try again!"
+    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+        return "You Lose! Scissor beats Paper"
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        return "You tied! Try again!"
+    } else if (playerSelection === "scissor" && computerSelection === "paper") {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection === "scissor" && computerSelection === "scissor") {
+        return "You tied! Try again!"
+    } else {
+        // (playerSelection === "scissor" && computerSelection === "rock")
+        return "You Lose! Paper beats Scissor"
+    }
 }
 
-computerPlay(options)
+
+console.log(playRound("papeR", "SciSSor"))
