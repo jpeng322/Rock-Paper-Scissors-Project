799c23a80dabd48cfa2d84fc9b476aee5695c732
jacky <jackypengsmail@gmail.com>
2022-05-08 01:18:36 -0400

    Show current and final score

--- index.js
+++ index.js
@@ -97,61 +97,27 @@ function computerPlay(a) {
 // console.log(computerPlay(options))
 
 
-let playerScore = 0
-let computerScore = 0
-
-function playRound(playerSelection, computerSelection) {
-    // playerSelection = playerSelection.toLowerCase();
-    // computerSelection = computerSelection.toLowerCase();
-    if (playerSelection === "rock" && computerSelection === "scissor") {
-        playerScore += 1
-        console.log(`Congratulations! You Won. The score is now ${playerScore} : ${computerScore}`)
-    } else if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore += 1
-        console.log(`You Lose! Paper beats Rock. The score is now ${playerScore} : ${computerScore}`)
-    } else if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log(`You tied! Try again! The score is now ${playerScore} : ${computerScore}`)
-    } else if (playerSelection === "paper" && computerSelection === "scissor") {
-        computerScore += 1
-        console.log(`You Lose! Scissor beats Paper. The score is now ${playerScore} : ${computerScore}`)
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore += 1
-        console.log(`Congratulations! You Won. The score is now ${playerScore} : ${computerScore}`)
-    } else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log(`You tied! Try again! The score is now ${playerScore} : ${computerScore}`)
-    } else if (playerSelection === "scissor" && computerSelection === "paper") {
-        playerScore += 1
-        console.log(`Congratulations! You Won. The score is now ${playerScore} : ${computerScore}`)
-    } else if (playerSelection === "scissor" && computerSelection === "scissor") {
-        console.log(`You tied! Try again! The score is now ${playerScore} : ${computerScore} `)
-    }
-    // (playerSelection === "scissor" && computerSelection === "rock")
-    else {
-        computerScore += 1
-        console.log(`You Lose! Rock beats Scissor. The score is now ${playerScore} : ${computerScore}`)
-    }
-}
 
 
-function game() {
-    let finalRes = ""
-    for (i = 0; i < 5; i++) {
-        const playerAns = prompt("What is your pick?").toLowerCase()
-        console.log(playerAns)
-        const options = ["Rock", "Paper", "Scissor"]
-        const computerAns = computerPlay(options).toLowerCase()
-        console.log(computerAns)
-        playRound(playerAns, computerAns)
-    }
-    if (playerScore > computerScore) {
-        finalRes = "Congratulations, you are the winner!"
-    } else if (playerScore < computerScore) {
-        finalRes = "Sorry, you are the loser!"
-    } else {
-        finalRes = "You tied! Try again!"
-    }
-    return `The final score is ${playerScore} : ${computerScore}! ${finalRes}`
-}
+// function game() {
+//     let finalRes = ""
+//     for (i = 0; i < 5; i++) {
+//         const playerAns = prompt("What is your pick?").toLowerCase()
+//         console.log(playerAns)
+//         const options = ["Rock", "Paper", "Scissor"]
+//         const computerAns = computerPlay(options).toLowerCase()
+//         console.log(computerAns)
+//         playRound(playerAns, computerAns)
+//     }
+//     if (playerScore > computerScore) {
+//         finalRes = "Congratulations, you are the winner!"
+//     } else if (playerScore < computerScore) {
+//         finalRes = "Sorry, you are the loser!"
+//     } else {
+//         finalRes = "You tied! Try again!"
+//     }
+//     return `The final score is ${playerScore} : ${computerScore}! ${finalRes}`
+// }
 // console.log(game())
 // console.log(playRound("papeR", "SciSSor"))
 
@@ -174,12 +140,14 @@ let computerAns = computerPlay(options).toLowerCase()
 rockBtn.addEventListener('click', function () {
     playRound("rock", computerAns)
     console.log(computerAns)
+    checkRes()
     computerAns = computerPlay(options).toLowerCase()
 })
 
 paperBtn.addEventListener('click', function () {
     playRound("paper", computerAns)
     console.log(computerAns)
+    checkRes()
     computerAns = computerPlay(options).toLowerCase()
 })
 
@@ -189,5 +157,61 @@ scissBtn.addEventListener('click', function () {
     console.log(computerAns)
     // console.log(playerSelection)
     // console.log(computerSelection)
+    checkRes()
     computerAns = computerPlay(options).toLowerCase()
 })
+
+let resultDiv = document.createElement('div')
+container.appendChild(resultDiv)
+
+resultDiv.textContent = ""
+
+let playerScore = 0
+let computerScore = 0
+
+function playRound(playerSelection, computerSelection) {
+    // playerSelection = playerSelection.toLowerCase();
+    // computerSelection = computerSelection.toLowerCase();
+    // if (playerScore === 5) {
+    //     return resultDiv.textContent = `Congratulations, you are the winner! The final score is ${playerScore} : ${computerScore}!`
+    // } else if (computerScore === 5) {
+    //     return resultDiv.textContent = `Sorry, you are the loser! The final score is ${playerScore} : ${computerScore}!`
+    // }
+    // else {
+    if (playerSelection === "rock" && computerSelection === "scissor") {
+        playerScore += 1
+        resultDiv.textContent = (`Congratulations! You Won. The score is now ${playerScore} : ${computerScore}`)
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore += 1
+        resultDiv.textContent = (`You Lose! Paper beats Rock. The score is now ${playerScore} : ${computerScore}`)
+    } else if (playerSelection === "rock" && computerSelection === "rock") {
+        resultDiv.textContent = (`You tied! Try again! The score is now ${playerScore} : ${computerScore}`)
+    } else if (playerSelection === "paper" && computerSelection === "scissor") {
+        computerScore += 1
+        resultDiv.textContent = (`You Lose! Scissor beats Paper. The score is now ${playerScore} : ${computerScore}`)
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1
+        resultDiv.textContent = (`Congratulations! You Won. The score is now ${playerScore} : ${computerScore}`)
+    } else if (playerSelection === "paper" && computerSelection === "paper") {
+        resultDiv.textContent = (`You tied! Try again! The score is now ${playerScore} : ${computerScore}`)
+    } else if (playerSelection === "scissor" && computerSelection === "scissor") {
+        playerScore += 1
+        resultDiv.textContent = (`Congratulations! You Won. The score is now ${playerScore} : ${computerScore}`)
+    } else if (playerSelection === "scissor" && computerSelection === "scissor") {
+        resultDiv.textContent = (`You tied! Try again! The score is now ${playerScore} : ${computerScore} `)
+    }
+    // (playerSelection === "scissor" && computerSelection === "rock")
+    else {
+        computerScore += 1
+        resultDiv.textContent = (`You Lose! Rock beats Scissor. The score is now ${playerScore} : ${computerScore}`)
+    }
+}
+// }
+
+function checkRes() {
+    if (playerScore === 5) {
+        return resultDiv.textContent = `Congratulations, you are the winner! The final score is ${playerScore} : ${computerScore}!`
+    } else if (computerScore === 5) {
+        return resultDiv.textContent = `Sorry, you are the loser! The final score is ${playerScore} : ${computerScore}!`
+    }
+}
